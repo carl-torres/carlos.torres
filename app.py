923c588b53cfb58a02adf34cc275df7641d41496
carl-torres <166522375+carl-torres@users.noreply.github.com>
2024-07-04 12:00:55 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+rgistro_pedido = []
+
+def registrar_pedido():
+    print("registro de pedido pedido")
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    comuna = input("Comuna ({}): ".format(", ".join(comuna)))
+    registrar_pedido=registrar_pedido
+    print(registrar_pedido)
+    
+
---
+++ funciones.py
@@ -0,0 +1,9 @@
+
+
+def registrar_pedido():
+    nombre=input("ingresar nombre")
+    apellido=input("ingresar apellido")
+    comuna=input("ingresar comuna")
+    detalle_pedidio=("detalle pedido")
+
+
